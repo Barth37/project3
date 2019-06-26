2ed976c01a5c33f69479b8426d04302378528f5e
Barth37 <46458549+Barth37@users.noreply.github.com>
2019-06-26 19:10:57 -0400

    Delete keys.js

--- passport-login/config/keys.js
+++
@@ -1,6 +0,0 @@
-//let mypass = "PeRrIn!37";
-//let dbPassword = 'mongodb+srv://Barth37:' + encodeURIComponent(mypass) + '@firstcluster-my9am.mongodb.net/test?retryWrites=true&w=majority';
-
-module.exports = {
-    MongoURI: 'mongodb+srv://Barth0037:PeRrIn37@cluster1-my9am.mongodb.net/test?retryWrites=true&w=majority'
-};
