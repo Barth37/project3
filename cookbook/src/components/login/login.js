a1a4a2e469fbe0efe94004d7dafa3cdb5f9777f7
picasso <46543347+djreichel@users.noreply.github.com>
2019-06-29 13:58:54 -0400

    updating

--- cookbook/src/components/login/login.js
+++ cookbook/src/components/login/login.js
@@ -12,11 +12,11 @@ export default class Login extends React.Component {
             <form class="log-in">
                 <p>Email</p>
                 <div class="ui input">
-                    <input type="text" placeholder="Email" value={this.state.username}/>
+                    <input type="text" placeholder="Email" value={this.state.email}/>
                 </div>
                 <p>Password</p>
                 <div class="ui input">
-                    <input type="text" placeholder="Password" value={this.state.email}/>
+                    <input type="text" placeholder="Password" value={this.state.password}/>
                 </div>
                 <br></br>
                 <button class="ui button">
