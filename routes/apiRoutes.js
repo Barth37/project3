1e662d5865131cd74208987e40d9d9f62050ecfc
picasso <46543347+djreichel@users.noreply.github.com>
2019-07-07 12:08:00 -0400

    updated apiRoutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,70 @@
-const axios = require("axios");
-const router = require("express").Router();
-const recipeController = require("../controllers/recipeController");
-const userController = require("../controllers/userController");
-var db = require("../models");
+const axios = require('axios');
+const router = require('express').Router();
+const recipeController = require('../controllers/recipeController');
+const userController = require('../controllers/userController');
+var db = require('../models');
 
-//user gets recipes from API - not authenticated
-router.get("/recipes", (req, res) => {
+// find all recipes based on keyword from third party api
+router.get('/recipes/:ingredient', (req, res) => {
+  console.log('apiRoutes is being reached');
+  const ingredient = req.params.ingredient;
+  var url = `https://api.edamam.com/search?q=${ingredient}&app_id=8b888a07&app_key=a9b6565744d4ce9f78b6d6fcd6448d54`;
   axios
-  .get("https://api.edamam.com/search?q=' + ingredient + '&app_id=8b888a07&app_key=a9b6565744d4ce9f78b6d6fcd6448d54&limit=10", { params: req.query })
-  .then(({ data: { results } }) => res.json(results))
-  .catch(err => res.status(422).json(err));
+    .get(url)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => res.status(422).json(err));
+  // .then(({ data: { results } }) => res.json(results))
+  // .catch(err => res.status(422).json(err));
 });
 
-router.route("/recipes")
-  .get(recipeController.findAll)
-  .get(recipeController.findById)
-  .post(recipeController.create)
-  .delete(recipeController.remove);
+//find all recipes in database
+router.get('/v1/recipes/', (req, res) => {
+  recipeController.findAll();
+});
 
+//find all recipes based on keyword in database
+router.get('/v1/recipes/:ingredient', (req, res) => {
+  recipeController.findAll();
+});
 
-router.get("/user", (req, res) => {
-  axios
-    .get("user", { params: req.query })
-    .then(({ data: { results } }) => res.json(results))
-    .catch(err => res.status(422).json(err));
-  });
-  
-router.route("/user")
-  .post(userController.create)
-  .get(userController.findById)
-  .put(userController.update)
-  .delete(userController.remove);
+//find one recipe based on keyword in database
+router.get('/v1/recipes/:id', (req, res) => {
+  var id = req.params.id;
+  recipeController.findById();
+});
+
+//save recipe
+router.put('/v1/recipes', (req, res) => {
+  recipeController.create();
+});
+
+//delete recipe
+router.delete('/v1/recipes/:id', (req, res) => {
+  recipeController.remove();
+});
+
+//create and save user
+router.post('/v1/user', (req, res) => {
+  userController.create();
+});
+
+// find one user
+router.get('/v1/user/:id', (req, res) => {
+  var id = req.params.id;
+  userController.findById();
+});
+//update user info
+router.put('/v1/user/:id', (req, res) => {
+  var id = req.params.id;
+  userController.update();
+});
+
+//delete user
+router.delete('/v1/user/:id', (req, res) => {
+  var id = req.params.id;
+  userController.remove();
+});
 
 module.exports = router;
