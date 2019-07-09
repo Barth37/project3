911a9cafded035f259fe6c2fbe297506c29dc1e7
picasso <46543347+djreichel@users.noreply.github.com>
2019-07-09 17:06:12 -0400

    updated user schema content

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  name: { type: String, trim: true, required: true },
+  email: { type: String, trim: true, required: true, unique: true, match:  [/.+@.+\..+/, "Please enter a valid e-mail address"]},
+  password: { type: String, trim: true, required: true },
   date: { type: Date, default: Date.now }
 });
 
