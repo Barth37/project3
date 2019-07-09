a8c1c6e5af5461b02ae01dbc0a19fb444c8136ae
picasso <46543347+djreichel@users.noreply.github.com>
2019-07-09 17:23:50 -0400

    updated server file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const apiRoutes = require('./routes/apiRoutes2');
+const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
