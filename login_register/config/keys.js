4afeb318a93b3113e952484ff1c73d114b7d4279
Barth37 <46458549+Barth37@users.noreply.github.com>
2019-07-10 17:16:46 -0400

    Update keys.js
    took out username and password

--- login_register/config/keys.js
+++ login_register/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://Barth0037:PeRrIn37@cluster1-my9am.mongodb.net/test?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://username:password@cluster1-my9am.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
